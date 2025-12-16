c38ee630197a3d6eda01e8b44a9b28a843782dbd
Aizhan <aizhanzhs@icloud.com>
2025-12-16 23:10:31 +0500

    Spring Boot

---
+++ src/main/java/org/example/zookeeper/dto/KeeperDTO.java
@@ -0,0 +1,4 @@
+package org.example.zookeeper.dto;
+
+public class KeeperDTO {
+}
---
+++ src/main/java/org/example/zookeeper/entity/Animal.java
@@ -0,0 +1,28 @@
+package org.example.zookeeper.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "animal_name")
+    private String name;
+    @Column(name = "animal_species")
+    private String species;
+    @Column(name = "animal_age")
+    private int age;
+
+@ManyToMany
+    private Set<Keeper> keepers = new HashSet<>();
+}
